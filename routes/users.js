b1eb67a6d17ef5fc34f47d4115eb2f0e8b96a995
GANOCHIR <user@USERnoMacBook-Air.local>
2026-05-05 13:19:09 +0900

    change login route

--- routes/users.js
+++ routes/users.js
@@ -5,28 +5,19 @@ const users = require("../controllers/users");
 
 router.route("/register").get(users.renderRegister).post(users.register);
 
-// router.post(
-//   "/login",
-//   passport.authenticate("local", {
-//     failureFlash: true,
-//     failureRedirect: "/login",
-//   }),
-//   (req, res) => {
-//     req.flash("success", "おかえりなさい！！");
-//     res.redirect("/campgrounds");
-//   }
-// );
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "おかえりなさい！！");
+    res.redirect("/campgrounds");
+  }
+);
 
-router
-  .route("/login")
-  .get(users.renderLogin)
-  .post(
-    passport.authenticate("local", {
-      failureFlash: true,
-      failureRedirect: "/login",
-    }),
-    users.login
-  );
+
 
 router.get("/logout", users.logout);
 
